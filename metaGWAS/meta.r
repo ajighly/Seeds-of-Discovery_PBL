676a620ee5df78c90937d5495637e2fc5e505f06
Abdulqader Jighly <37642811+ajighly@users.noreply.github.com>
2020-11-26 01:17:20 +1100

    Create meta.r

---
+++ metaGWAS/meta.r
@@ -0,0 +1,6 @@
+Ts=read.table('Ts.txt')
+n=ncol(Ts)
+pvalues=matrix(NA,nrow=nrow(Ts),ncol=1)
+V=solve(cor(Ts))
+for (i in 1:nrow(Ts)) pvalues[i,1]=pchisq(as.matrix(t(Ts[i,]))%*%V%*%as.matrix(Ts[i,]), df=n, lower.tail=FALSE)
+write.table(pvalues,'metaAll',quote=F,sep='\t')
